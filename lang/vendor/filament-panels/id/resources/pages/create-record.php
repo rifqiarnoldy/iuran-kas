c35380601324b7b6bee6f0f4e5a56681e38f09cd
rifqiarnoldy <rifqiarnoldy@gmail.com>
2024-08-31 00:48:25 +0700

    add translation

---
+++ lang/vendor/filament-panels/id/resources/pages/create-record.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+
+    'title' => 'Buat Data :label',
+
+    'breadcrumb' => 'Buat Data',
+
+    'form' => [
+
+        'actions' => [
+
+            'cancel' => [
+                'label' => 'Batal',
+            ],
+
+            'create' => [
+                'label' => 'Buat Data',
+            ],
+
+            'create_another' => [
+                'label' => 'Buat & buat lainnya',
+            ],
+
+        ],
+
+    ],
+
+    'notifications' => [
+
+        'created' => [
+            'title' => 'Data berhasil dibuat',
+        ],
+
+    ],
+
+];
